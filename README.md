b0520a7ce7c03c3db0d346e9451e62592d628724
weigang.wang <weigang.wang5@gmail.com>
2022-08-25 19:56:11 +0100

    commit all pwa related changes

---
+++ README.md
@@ -0,0 +1,58 @@
+# Convert a React App into a Progressive Web App (PWA)
+
+In addition to behave as a Web App, A Progressive Web App (PWA) can also be accessed offline after the first access with network connection, which would make it cached in your device. You may add it to the home screen of your device as a launch icon (this is called PWA installation that makes its access similar to installed native mobile app).
+
+You may convert an existing React Web App into a PWA through the following steps (lets take the [TicTacToe Web app](https://github.com/wadehonda/tictactoe/tree/master) as an example):
+
+1. Create a React App with a PWA Template:
+
+`npx create-react-app tictactoe-pwa --template cra-template-pwa`
+
+2. Register a Service Worker
+
+   The React app created with the above mentioned template has a `service-worker.js` in the root directory. You can register the service worker by changing the statement of `serviceWorker.unregister()` to `serviceWorker.register()` in the `index.js` file in the root directory.
+
+3. Configure the Web Application Manifest
+
+   In the `public` directory, there is a `manifect.json` file, we may modify it with our own app name and our needed configurations, e.g.,
+
+   ```
+    {
+      "short_name": "tictactoe-pwa",
+      "name": "tictactoe-pwa",
+      "icons": [
+        {
+          "src": "favicon.ico",
+          "sizes": "64x64 32x32 24x24 16x16",
+          "type": "image/x-icon"
+        },
+      ],
+      "start_url": ".",
+      "display": "fullscreen",
+      "theme_color": "#000000",
+      "background_color": "#ffffff"
+    }
+   ```
+
+   The `fullscreen` value of the `display` property makes the app running in fullscreen mode with a native app look-and-feel, without the browser UI elements like an address bar.
+
+4. Copy the Code of an Existing App into this New App
+
+   In this case, you may paste the tictactoe code into the `App.js` file; and paste the `CSS` style code of the game into `index.css`. As the game code used two react icons, you need to add this library into the project dependency:
+
+   `npm install react-icons --save`
+
+5. Now, You Can Build the PWA:
+
+   `npm run build`
+
+   The build folder is now ready to be deployed.
+
+6. You may Test the PWA App with a Static Server:
+
+   `serve -s build`
+
+7. You may Analyse the PWA App using Chrome DevTools
+
+   You may analyse the performance, accessibility, best practices, SEO, and PWA aspects of your app using the Chrome DevTools by selecting the `Inspect` option from the right-click menu, and then selecting the `Lighthouse` tab (more tabs in the Inspect window can be found by clicking on `>>` tab), selecting the `Navigation` mode and `Mobile` device, making all the categories in Lighthouse window ticked, and then clicking `Analyze page load` button. A report with scores on all the checked categories will be displayed. You may modify your app based on the feedback to improve it in multiple refinement and analysis cycles.
+   To learn more, visit [https://cra.link/PWA](https://cra.link/PWA)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,185 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
 
-function App() {
+function Square(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <button
+      style={{
+        color: props.lastSquare ? "red" : "black",
+        background: props.winningSquare ? "green" : "white",
+      }}
+      className="square"
+      onClick={props.onChange}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+function Board(props) {
+  const rowLayout = (i) => {
+    const row = gameLines[i];
+    return (
+      <div className="board-row">
+        {row.map((x) => (
+          <Square
+            key={x}
+            value={props.squares[x]}
+            lastSquare={props.lastSquare === x}
+            winningSquare={props.winningSquares?.includes(x)}
+            onChange={() => props.onChange(x)}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const content = [0, 1, 2].map((x) => rowLayout(x));
+  return <div>{content}</div>;
+}
+
+export default function Game() {
+  const end = useRef(false);
+  const nextStone = useRef("X");
+  const [record, setRecord] = useState([{ squares: Array(9).fill(null) }]);
+  const [move, setMove] = useState(0);
+  const [index, setIndex] = useState(0);
+
+  let currentSquares = record[index].squares;
+  let lastSquare = record[index].lastSquare;
+
+  const reset = () => {
+    end.current = false;
+    setRecord([{ squares: Array(9).fill(null), lastSquare: null }]);
+    nextStone.current = "X";
+    setMove(0);
+    setIndex(0);
+  };
+
+  const next = () => {
+    if (index < record.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
+  const prev = () => {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  };
+  const goTo = (step) => {
+    setIndex(step);
+    if (step % 2 === 0) {
+      nextStone.current = "X";
+    } else {
+      nextStone.current = "O";
+    }
+  };
+
+  const handleChange = (i) => {
+    if (!end.current) {
+      const temp = currentSquares.slice();
+      if (currentSquares[i] == null) {
+        temp[i] = nextStone.current;
+        nextStone.current =
+          nextStone.current === "X"
+            ? (nextStone.current = "O")
+            : (nextStone.current = "X");
+        setRecord([...record, { squares: temp, lastSquare: i }]);
+        setMove(move + 1);
+        setIndex(index + 1);
+      } else {
+        alert("illegal move");
+      }
+    }
+  };
+
+  const squares = currentSquares.slice();
+  let winner = gameEnding(squares);
+  let winningSquares = null;
+  let result = null;
+  if (winner) {
+    end.current = true;
+    result = winner[0] + " win";
+    winningSquares = gameLines[winner[1]];
+  } else if (!currentSquares.includes(null)) {
+    end.current = true;
+    result = "Draw";
+  }
+  const status = !end.current ? "Next move is " + nextStone.current : result;
+
+  const moves = record.map((step, move) => {
+    const ls = step.lastSquare;
+    const desc = move ? "#" + move + " in square " + ls : "Go to game start";
+    return (
+      <li key={move}>
+        <button
+          style={{ color: lastSquare === ls ? "red" : "black" }}
+          onClick={() => goTo(move)}
         >
-          Learn React
-        </a>
-      </header>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <h3>TicTacToe PWA</h3>
+        <Board
+          lastSquare={lastSquare}
+          winningSquares={winningSquares}
+          squares={currentSquares}
+          onChange={(i) => handleChange(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        {end.current && (
+          <div>
+            <div>
+              <button onClick={() => reset()}>New game</button>
+            </div>
+            <div>
+              <p>Replay:</p>
+              <button onClick={() => prev()}>
+                <BsFillCaretLeftFill />
+                Back
+              </button>
+              <button onClick={() => next()}>
+                Forward
+                <BsFillCaretRightFill />
+              </button>
+            </div>
+
+            <div>
+              <ol>{moves}</ol>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+const gameLines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function gameEnding(squares) {
+  for (let i = 0; i < gameLines.length; i++) {
+    const [a, b, c] = gameLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [squares[a], i];
+    }
+  }
+  return null;
+}
